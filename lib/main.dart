c8bdde435e102e64d6a77e6c84589cc1cebdfec0
Anderson Boeing <anderboeing@gmail.com>
2026-07-06 17:38:37 -0300

    TextField e decoração

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,31 @@ class _HomeState extends State<Home> {
         title: Text('Calculadora IMC'),
         centerTitle: true,
         backgroundColor: Colors.green,
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.refresh), onPressed: () {}),
+        actions: [IconButton(icon: Icon(Icons.refresh), onPressed: () {})],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Peso (kg)",
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Altura (cm)",
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
         ],
       ),
     );
