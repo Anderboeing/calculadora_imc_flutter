961c89d9d6079b8652762c6e424c7f2c37a054ee
Anderson Boeing <anderboeing@gmail.com>
2026-07-06 17:26:08 -0300

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculadora IMC'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: () {}),
+        ],
+      ),
+    );
+  }
+}
