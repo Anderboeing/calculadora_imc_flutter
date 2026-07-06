8c11d9086f2ff29aa1585bbeb7162db65b115e57
Anderson Boeing <anderboeing@gmail.com>
2026-07-06 18:29:02 -0300

    Controller e cálculos

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,40 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _infoText = "Informe seus dados!";
+
+  void _resetFields() {
+    weightController.text = "";
+    heightController.text = "";
+    _infoText = "Informe seus dados!";
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height =
+          double.parse(heightController.text) /
+          100; // Convertendo de cm para metros
+      double imc = weight / (height * height);
+      debugPrint(imc.toString());
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso (${imc.toStringAsFixed(2)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso ideal (${imc.toStringAsFixed(2)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do peso (${imc.toStringAsFixed(2)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade grau I (${imc.toStringAsFixed(2)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade grau II (${imc.toStringAsFixed(2)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade grau III (${imc.toStringAsFixed(2)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +53,9 @@ class _HomeState extends State<Home> {
         title: Text('Calculadora IMC'),
         centerTitle: true,
         backgroundColor: Colors.green,
-        actions: [IconButton(icon: Icon(Icons.refresh), onPressed: () {})],
+        actions: [
+          IconButton(icon: Icon(Icons.refresh), onPressed: _resetFields),
+        ],
       ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
@@ -36,6 +72,7 @@ class _HomeState extends State<Home> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.0),
+              controller: weightController,
             ),
             TextField(
               keyboardType: TextInputType.number,
@@ -45,6 +82,7 @@ class _HomeState extends State<Home> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.0),
+              controller: heightController,
             ),
             Padding(
               padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
@@ -58,12 +96,12 @@ class _HomeState extends State<Home> {
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.green,
                   ),
-                  onPressed: () {},
+                  onPressed: _calculate,
                 ),
               ),
             ),
             Text(
-              'info',
+              _infoText,
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.0),
             ),
